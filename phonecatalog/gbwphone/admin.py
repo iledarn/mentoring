de51ad27c8c389dd528d85d98fabb60ac2e8aabd
iledarn <iledarn@gmail.com>
2022-09-11 21:29:44 +0800

    add - main admin for PhoneCatalog model

--- phonecatalog/gbwphone/admin.py
+++ phonecatalog/gbwphone/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from gbwphone.models import PhoneCatalog
+
+
+@admin.register(PhoneCatalog)
+class PhoneCatalogAdmin(admin.ModelAdmin):
+    enable_nav_sidebar = False
+    list_display = [
+        "view_id",
+        "view_admin_slug",
+        "active",
+    ]
+
+    def view_admin_slug(self, obj):
+        return obj.admin_slug
+
+    view_admin_slug.short_description = "Name"
+
+    def view_id(self, obj):
+        return obj.id
+
+    view_id.short_description = "Product ID"
